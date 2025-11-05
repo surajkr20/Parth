4129ab1cddd31654b264f7a7b3e770bcb5e380a1
surajkr20 <suraj71308kumar@gmail.com>
2025-11-05 11:11:24 +0530

    error debugging - and again did frontend setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
+      </Routes>
+    </div>
   )
 }
 
-export default App
+export default App;
