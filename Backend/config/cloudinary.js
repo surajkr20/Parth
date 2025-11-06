7710be8547b81c9d120b550246ce6208e2d433f0
surajkr20 <suraj71308kumar@gmail.com>
2025-11-06 11:02:08 +0530

    cloudinary and multer setup for image upload

---
+++ Backend/config/cloudinary.js
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+export const uploadOnCloudinary = async (FilePath) => {
+  // Configuration
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.API_SECRET,
+  });
+
+  // Upload an image
+  try {
+    const uploadResult = await cloudinary.uploader.upload(FilePath);
+    fs.unlinkSync(FilePath);
+    return uploadResult.secure_url;
+  } catch (error) {
+    fs.unlinkSync(FilePath);
+    return res.status(500).json({message: `cloudinary error ${error}`})
+  }
+};
+
+export default uploadOnCloudinary;
