4902acd0b2f28781b6ac30ced2803447612e6964
surajkr20 <suraj71308kumar@gmail.com>
2025-11-09 11:47:30 +0530

    askToAssistant controller and route created

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,5 +1,7 @@
 import userModel from "../models/user.model.js";
 import { uploadOnCloudinary } from "../config/cloudinary.js";
+import geminiResponse from "../gemini.js";
+import moment from "moment/moment.js";
 
 export const getCurrentUser = async(req, res) =>{
     try {
@@ -29,5 +31,68 @@ export const updateAssistant = async(req, res) =>{
         return res.status(200).json(user);
     } catch (error) {
         return res.status(500).json({message: `update Assistant error: ${error}`});
+    }
+}
+
+export const askToAssistant = async(req, res) =>{
+    try {
+        const {command} = req.body;
+        const user = await userModel.findById(req.userId);
+        const userName = user.name;
+        const assistantImage = user.assistantImage;
+        const assistantName = user.assistantName;
+        const result = await geminiResponse(command, userName, assistantName);
+        const jsonMatch = result.match(/{[\s\S]*}/);
+        if(!jsonMatch){
+            res.status(400).json({response: "sorry i can't understand"})
+        }
+        const gemResult = JSON.parse(jsonMatch[0]);
+        const type = gemResult.type;
+
+        switch(type){
+            case 'get-date': 
+                return res.json({
+                    type,
+                    userInput: gemResult.userInput,
+                    response: `current date is ${moment().format("YYYY-MM-DD")}`
+                })
+            case 'get-time':
+                return res.json({
+                    type,
+                    userInput: gemResult.userInput,
+                    response: `current time is ${moment().format("hh-mm A")}`
+                })
+            case 'get-day': 
+                return res.json({
+                    type,
+                    userInput: gemResult.userInput,
+                    response: `today is ${moment().format("dddd")}`
+                })
+            case 'get-month': 
+                return res.json({
+                    type,
+                    userInput: gemResult.userInput,
+                    response: `current month is ${moment().format("MMMM")}`
+                })
+            case 'google_search':
+            case 'youtube_search':
+            case 'instagram_open':
+            case 'youtube_play':
+            case 'facebook_open':
+            case 'weather_show':
+            case 'calculator_open':
+            case 'project_summary':
+                return res.json({
+                    type,
+                    userInput: gemResult.userInput,
+                    response: gemResult.response
+                })
+            
+            default : 
+                return res.status(400).json({message: "i didn't understand that command!"})
+        }
+
+    } catch (error) {
+        return res.status(500).json({message: "asktoAssistant controller error!"})
     }
 }
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 const userRouter = express.Router();
-import { getCurrentUser, updateAssistant } from "../controllers/user.controller.js";
+import { askToAssistant, getCurrentUser, updateAssistant } from "../controllers/user.controller.js";
 import isAuth from "../middlewares/isAuth.js";
 import upload from "../middlewares/multer.js";
 
 userRouter.get('/me', isAuth, getCurrentUser);
 userRouter.post('/update', isAuth, upload.single("assistantImage"), updateAssistant);
+userRouter.post('/asktoassistant', isAuth, askToAssistant);
 
 export default userRouter;
