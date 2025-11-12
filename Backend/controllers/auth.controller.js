b1b7bc3d016237dbc3c7a6dfe7d64cefafeac4a6
surajkr20 <suraj71308kumar@gmail.com>
2025-11-12 14:33:54 +0530

    frontend url replaced with deployed frontend url

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -23,8 +23,8 @@ export const signup = async(req, res) =>{
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7*24*60*60*1000,
-            sameSite: "strict",
-            secure: false
+            sameSite: "None",
+            secure: true
         });
         return res.status(201).json({user, token});
     } catch (error) {
@@ -51,8 +51,8 @@ export const login = async(req, res) =>{
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7*24*60*60*1000,
-            sameSite: "strict",
-            secure: false
+            sameSite: "None",
+            secure: true
         });
         return res.status(200).json({user, token});
     } catch (error) {
--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://parth-virtualassistant.onrender.com",
   credentials: true
 }));
 
